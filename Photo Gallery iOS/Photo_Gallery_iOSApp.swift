42c0719f86e204f524bae33c73f3f9be36b2380d
Farial Mahmod <farialmahmod@yahoo.com>
2023-10-03 09:05:21 -0700

    Initial Commit

---
+++ Photo Gallery iOS/Photo_Gallery_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_Gallery_iOSApp.swift
+//  Photo Gallery iOS
+//
+//  Created by Farial Mahmod on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Photo_Gallery_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
