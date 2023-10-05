bf9ee78be88e5ca26bdfaec182d736fab524adb5
Farial Mahmod <farialmahmod@yahoo.com>
2023-10-04 23:52:38 -0700

    Moving the keys to a different folder and using enum to access the keys elsewhere

---
+++ Photo Gallery iOS/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Photo Gallery iOS
+//
+//  Created by Farial Mahmod on 10/4/23.
+//
+
+import Foundation
+
+enum Constants {
+    static let UnsplashKey = "-2kFD_PDKY_EFCfQuv5Fb4xfiz6lK9qOxFPf_QI_ueM"
+    static let UnsplashSecretCode = "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"
+}
--- Photo Gallery iOS/ViewModel/APIFormation.swift
+++ Photo Gallery iOS/ViewModel/APIFormation.swift
@@ -16,11 +16,9 @@ init(){
 
 // loadPhotos() initiates the API request to the Unsplash server
 func loadPhotos(){
-    
-    let key = "-2kFD_PDKY_EFCfQuv5Fb4xfiz6lK9qOxFPf_QI_ueM"
-    // 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
+
     // URL provided by Unplash doc
-    guard let url = URL(string: "https://api.unsplash.com/photos/random/?count=719&client_id=\(key)") else {return }
+    guard let url = URL(string: "https://api.unsplash.com/photos/random/?count=719&client_id=\(Constants.UnsplashKey)") else {return }
 
     // API Call
     URLSession.shared.dataTask(with: url) { (data, response, error) in
